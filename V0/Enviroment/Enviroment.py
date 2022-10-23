6ca5cf34a2637911384b70bb56d40185dc3087cc
medis8731 <56053205+medis8731@users.noreply.github.com>
2022-10-23 15:35:07 -0500

    Updated Enviroment.py
    After this update the obstacles don't crash with the start and end anymore.

--- V0/Enviroment/Enviroment.py
+++ V0/Enviroment/Enviroment.py
@@ -1,8 +1,7 @@
 import random
-from turtle import circle, color
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle, Polygon, Rectangle
-
+import numpy as np
 class Enviroment:
     def __init__(self, start, goal, MapDimensions, obsdim, obsnum):
         #Set locations and dimensions
@@ -28,10 +27,46 @@ class Enviroment:
         uppercornery = int(random.uniform(1, self.maph -  self.obsdim[1]))
         return (uppercornerx, uppercornery)
 
-    def createRandomObstacles(self):
+    def createRandomObstaclesV0(self):
         for i in range(0,self.obsnum):
             self.obs.append(self.makeRandomRect())
-    
+
+    def createRandomObstacles(self):
+        obs = []
+        for i in range(0, self.obsnum):
+            rectang = None
+            startgoalcol = True
+            while startgoalcol:
+                rectangCor = self.makeRandomRect()
+                rectang = Rectangle((rectangCor[0], rectangCor[1]), self.obsdim[1], self.obsdim[0])
+                center = rectang.get_center()
+                if self.circleInterRec(self.start[0],self.start[1],self.startR,center[0],center[1],self.obsdim[1], self.obsdim[0]) \
+                     or self.circleInterRec(self.goal[0],self.goal[1],self.goalR,center[0],center[1],self.obsdim[1], self.obsdim[0]):
+                    startgoalcol = True
+                else:
+                    startgoalcol = False
+            obs.append(rectangCor)
+        self.obs = obs.copy()
+        return obs
+
+    def recConrner(self,rectangle):
+        coords = rectangle.get_bbox().get_points()
+        return (coords[0][0],coords[1][1],coords[1][0],coords[0][1])
+
+    def circleInterRec(self,cirCenterX,cirCenterY,cirR,rectX,rectY,recH,recW):
+        circleDistanceX = abs(cirCenterX - rectX)
+        circleDistanceY = abs(cirCenterY - rectY)
+
+        if (circleDistanceX > (recW/2 + cirR)):  return False
+        if (circleDistanceY > (recH/2 + cirR)) :  return False
+
+        if (circleDistanceX <= (recW/2)): return True
+        if (circleDistanceY <= (recH/2)) : return True
+
+        cornerDistance_sq = (circleDistanceX - recW/2)^2 +(circleDistanceY - recH/2)**2
+
+        return (cornerDistance_sq <= (cirR**2))
+
     def showMap(self):
         self.fig ,self.ax = plt.subplots()
         self.ax.set_xlim(0,env.mapw)
